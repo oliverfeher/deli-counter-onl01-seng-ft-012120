b5d0971cfe1e9b4323d5871555ace849287139df
Oliver Feher <olcsee@gmail.com>
2020-01-23 05:20:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,11 @@ katz_deli = []
 
 def line(array)
   if array.length > 0
+    array1 = []
     counter = 1
-    puts ""
+    array.each do |name|
+      array1.push("#{counter}. #{name}")
+      counter +=1
   else
     puts "The line is currently empty."
   end
